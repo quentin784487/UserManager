3a4b627df5adf128773ea400316a89a3992053c4
quentinwebtonic <quentin@webtonic.co.za>
2023-03-22 19:16:31 +0200

    Unit test with mocking added

--- UserManager.Tests/UserTests.cs
+++ UserManager.Tests/UserTests.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using UserManager.API.Controllers;
+using UserManager.API.Models.User;
 using UserManager.Core.Domain.Entities;
 using UserManager.Infrastructure.Entities;
 using UserManager.Service.Contracts;
@@ -9,26 +10,25 @@ using UserManager.Service.Contracts;
 namespace UserManager.Tests;
 
 public class UserTests
-{    
+{
     [Fact]
-    public async void Get_WhenCalled_ReturnsOkResult()
-    {        
+    public void Get_WhenCalled_ReturnsOkResult()
+    {
         //Arrange
-        long userId = new Random().NextInt64();
+        int userId = new Random().Next();
+
         var user = new User { Id = userId, Firstname = "firstname", Lastname = "lastname", Email = "email", Username = "username", Password = "password", IsActive = true, CreatedBy = "createdby", CreatedDate = DateTime.Now, ModifiedBy = "modifiedby", ModifiedDate = DateTime.Now };
 
         var mockUserService = new Mock<IUserService>();
 
         mockUserService.Setup(x => x.GetByIdAsync(It.Is<long>(i => i == userId))).Returns(user);
-
-        var config = new MapperConfiguration(cfg =>
-        {            
-            cfg.CreateMap<UserCore, User>();
-        });
+        
+        var config = new MapperConfiguration(cfg => {  cfg.CreateMap<User, UserViewModel > (); });
 
         // Act
         var userController = new UserController(new Mapper(config), mockUserService.Object);
-        var okResult = await userController.GetAllAsync();
+
+        var okResult = userController.Get(userId);
 
         // Assert
         Assert.IsType<OkObjectResult>(okResult as OkObjectResult);
